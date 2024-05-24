f8a72d39da1da54d4b3ae930ba696f8890ffc748
CarlosEduFF <carlos.farias11@etec.sp.gov.br>
2024-05-24 17:41:48 -0300

    Listar Atualizado

---
+++ app/src/main/java/viewModel/PessoaViewModel.kt
@@ -0,0 +1,23 @@
+package viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.databaseapp.RoomDB.Pessoa
+import kotlinx.coroutines.launch
+
+class PessoaViewModel(private val repository: Repository): ViewModel() {
+    fun getPessoa() = repository.getAllPessoa().asLiveData(viewModelScope.coroutineContext)
+
+    fun upsertPessoa(pessoa: Pessoa){
+        viewModelScope.launch{
+            repository.upsertPessoa(pessoa)
+        }
+    }
+
+    fun deletePessoa(pessoa: Pessoa){
+        viewModelScope.launch{
+            repository.deletePessoa(pessoa)
+        }
+    }
+}
